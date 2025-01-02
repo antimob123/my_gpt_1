94df74c028e710bade2ce175add1dd7e01ceb737
antimob123 <112704512+antimob123@users.noreply.github.com>
2025-01-02 15:38:26 +0800

    1
    1

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ for message in st.session_state["messages"]:
 prompt = st.chat_input()
 if prompt:
     if not openai_api_key:
-#        st.info("请输入你的OpenAI API Key")
-        # st.stop()
-    st.session_state["messages"].append({"role": "human", "content": prompt})
-    st.chat_message("human").write(prompt)
+     #st.info("请输入你的OpenAI API Key")
+     # st.stop()
+        st.session_state["messages"].append({"role": "human", "content": prompt})
+        st.chat_message("human").write(prompt)
 
     with st.spinner("AI正在思考中，请稍等..."):
         response = get_chat_response(prompt, st.session_state["memory"],
